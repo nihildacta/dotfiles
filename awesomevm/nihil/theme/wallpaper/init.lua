cd1f951141e041e64246120dc18f60b8a895e2dc
Ricardo Batista <emacs.copo@gmail.com>
2018-07-08 00:13:56 +0100

    Move theme to another file

---
+++ awesomevm/nihil/theme/wallpaper/init.lua
@@ -0,0 +1,17 @@
+local gears = require("gears")
+local beautiful = require("beautiful")
+local wallpaper = {}
+
+function wallpaper.set_wallpaper(s)
+    -- Wallpaper
+    if beautiful.wallpaper then
+        local wallpaper = beautiful.wallpaper
+        -- If wallpaper is a function, call it with the screen
+        if type(wallpaper) == "function" then
+            wallpaper = wallpaper(s)
+        end
+        gears.wallpaper.maximized(wallpaper, s, true)
+    end
+end
+
+return wallpaper
--- awesomevm/rc.lua
+++ awesomevm/rc.lua
@@ -6,6 +6,7 @@ local beautiful = require("beautiful")
 local naughty = require("naughty")
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup").widget
+local wallpaper =  require("nihil.theme.wallpaper")
 require("awful.hotkeys_popup.keys.vim")
 require("nihil.exception.handler")
 
@@ -104,23 +105,13 @@ local tasklist_buttons = gears.table.join(
     end)
 )
 
-local function set_wallpaper(s)
-    -- Wallpaper
-    if beautiful.wallpaper then
-        local wallpaper = beautiful.wallpaper
-        -- If wallpaper is a function, call it with the screen
-        if type(wallpaper) == "function" then
-            wallpaper = wallpaper(s)
-        end
-        gears.wallpaper.maximized(wallpaper, s, true)
-    end
-end
+
 
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
-screen.connect_signal("property::geometry", set_wallpaper)
+screen.connect_signal("property::geometry", wallpaper.set_wallpaper)
 
 awful.screen.connect_for_each_screen(function(s)
-    set_wallpaper(s)
+    wallpaper.set_wallpaper(s)
 
     -- Each screen has its own tag table.
     awful.tag({ "d", "w", "m", "x" }, s, awful.layout.layouts[1])
